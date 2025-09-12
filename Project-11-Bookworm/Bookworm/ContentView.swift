b95329cb88c898f3f0d62b37d8e6d9a3862cec51
Gregory <gloaming@gmail.com>
2025-09-12 13:21:36 -0700

    initial commit of Bookworm

---
+++ Project-11-Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Gregory Randolph on 9/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
